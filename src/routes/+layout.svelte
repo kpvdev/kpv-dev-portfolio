574626091ae122ef0c43b4e6207d58f683ed890c
Kyle P. Vincent <110914225+kpvdev@users.noreply.github.com>
2023-02-16 15:45:23 +0000

    Removed flowbite-svelte footer / applied styles

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,7 +4,7 @@
 	import { Footer, FooterLinkGroup, FooterLink, ImagePlaceholder, TextPlaceholder, Skeleton, FooterCopyright } from 'flowbite-svelte'
 	import Navbar from '../components/Navbar.svelte';
 	import SocialBanner from '../components/SocialBanner.svelte';
-	// import './styles.css';
+	import './styles.css';
 </script>
 
 <Header />
@@ -12,17 +12,5 @@
 <main>
 	<slot />
 </main>
-
-<Footer class="bottom-0 left-0 z-20 w-full">
-  <FooterCopyright href="/" by="Flowbite™" year={2022} />
-  <FooterLinkGroup
-    ulClass="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0"
-  >
-    <FooterLink href="/">About</FooterLink>
-    <FooterLink href="/">Privacy Policy</FooterLink>
-    <FooterLink href="/">Licensing</FooterLink>
-    <FooterLink href="/">Contact</FooterLink>
-  </FooterLinkGroup>
-</Footer>
 
 <SocialBanner />
