40e369157ef9ce00a708aeebce996c7720d762af
Kyle P. Vincent <110914225+kpvdev@users.noreply.github.com>
2023-02-16 19:24:00 +0000

    Config changes for Vercel deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 
-/** @type {import('@sveltejs/kit').Config} */
+/** @type {import('@sveltejs/adapter-vercel').Config} */
 const config = {
 	kit: {
 		adapter: adapter()
